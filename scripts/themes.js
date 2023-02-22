1f7dc7a43ee36d51138d012db97aa036dbd1650a
etetdev <112492696+etetdev@users.noreply.github.com>
2023-02-22 13:05:05 +0100

    Update 0.1.1

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,50 +1,50 @@
-// select the icons
+// Select the icons
 const lightIcon = document.querySelector('.light');
 const darkIcon = document.querySelector('.dark');
 
-// get the user's theme choice from the local storage
+// Get the user's theme choice from the local storage
 const userTheme = localStorage.getItem('theme');
 
-// check if the user has a dark theme preference
+// Check if the user has a dark theme preference
 const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-// toggle the visibility of the icons
+// Toggle the visibility of the icons
 const iconToggle = () => {
 	darkIcon.classList.toggle('display-none');
 	lightIcon.classList.toggle('display-none');
 };
 
-// check the user's theme choice and apply it
+// Check the user's theme choice and apply it
 const themeCheck = () => {
-	// if the user has a dark theme preference, apply it
+	// If the user has a dark theme preference, apply it
 	if (userTheme === 'dark' || (!userTheme && systemTheme)) {
 		document.documentElement.classList.add('dark');
 		darkIcon.classList.add('display-none');
 		return;
 	}
-	// if the user has a light theme preference, apply it
+	// If the user has a light theme preference, apply it
 	lightIcon.classList.add('display-none');
 };
 
-// toggle the theme and save the user's choice
+// Toggle the theme and save the user's choice
 const themeSwitch = () => {
-	// if the user has a dark theme, change to light
+	// If the user has a dark theme, change to light
 	if (document.documentElement.classList.contains('dark')) {
 		document.documentElement.classList.remove('dark');
 		localStorage.setItem('theme', 'light');
 		iconToggle();
 		return;
 	}
-	// if the user has a light theme, change to dark
+	// If the user has a light theme, change to dark
 	document.documentElement.classList.add('dark');
 	localStorage.setItem('theme', 'dark');
 	iconToggle();
 };
 
-// apply the user's theme choice on page load
+// Apply the user's theme choice on page load
 themeCheck();
 
-// add click event listeners to the icons
+// Add click event listeners to the icons
 lightIcon.addEventListener('click', () => {
 	themeSwitch();
 });
